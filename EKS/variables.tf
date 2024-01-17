c1469ba181b4cf23dc706b4d4c1216910022e790
kushalsrihari <76680565+kushalsrihari@users.noreply.github.com>
2024-01-17 09:18:39 +0530

    Create variables.tf

---
+++ EKS/variables.tf
@@ -0,0 +1,11 @@
+variable "ami" {
+  description = "AMI ID for EC2 instances"
+}
+
+variable "instance_type" {
+  description = "Instance type for EC2 instances"
+}
+
+variable "key_name" {
+  description = "Key pair name for EC2 instances"
+}
